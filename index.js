987f1c5ead8f37ade4317fc57cbf66b3bda6f402
Mischel <susan.mwaura@student.moringaschool.com>
2023-06-24 07:56:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function receivesAFunction(callback) {
+  
+    callback(); // Call the received function
+  }
+  
+
+  // Import necessary dependencies and modules
+
+// Test suite
+function returnsANamedFunction() {
+  // Function implementation
+  // ...
+   // Function implementation
+   let exercise = () => {
+    // Code for the exercise function
+  };
+
+  let myFunction = exercise;
+  return myFunction;
+}
+function returnsAnAnonymousFunction() {
+  return () => {
+    // Function implementation
+    // ...
+  };
+}
+
+// Rest of the code...
+
+// describe('returnsANamedFunction', () => {
+//     let myFunction;
+  
+//     // Before all hook
+//     before(() => {
+//       // Set up any necessary resources or perform any setup actions here
+//       myFunction = returnsANamedFunction();
+//     });
+  
+    // Test case
+    it('should return a named function', () => {
+      // Assertions or expectations for the test case
+      // Use myFunction and perform necessary checks
+      // ...
+    });
+  
+  
